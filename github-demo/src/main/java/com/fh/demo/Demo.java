d1dd793b5fc986353958ae793fa1854f9d58336f
zhen-xin <w319257383@163.com>
2020-01-03 11:57:32 +0800

    第三次上传

--- github-demo/src/main/java/com/fh/demo/Demo.java
+++ github-demo/src/main/java/com/fh/demo/Demo.java
@@ -9,4 +9,9 @@ public class Demo {
     public String demo(){
         return "张毅恒我是你爹!!!你是我儿!!!";
     }
+
+    @RequestMapping("test")
+    public String test(){
+        return "牛润之我是你爹!!!你是我儿!!!";
+    }
 }
